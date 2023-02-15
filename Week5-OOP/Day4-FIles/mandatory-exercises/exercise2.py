b7714536f250ea6e22dbe8d5752d6e4f2b1987a2
Avishay <avtwito@gmail.com>
2023-02-15 16:29:37 +0200

    day3 mandatory exercises

--- Week5-OOP/Day4-FIles/mandatory-exercises/exercise2.py
+++ Week5-OOP/Day4-FIles/mandatory-exercises/exercise2.py
@@ -1,5 +1,15 @@
 import json
-sampleJson = """{ "company":{ "employee":{ "name":"emma","payable":{ "salary":7000,"bonus":800}}}}"""
+sampleJson = """{ 
+   "company":{ 
+      "employee":{ 
+         "name":"emma",
+         "payable":{ 
+            "salary":7000,
+            "bonus":800
+         }
+      }
+   }
+}"""
 
 # Access the nested “salary” key from the JSON-string above.
 sample = json.loads(sampleJson)
@@ -7,5 +17,4 @@ print(sample["company"]["employee"]["payable"]["salary"])
 # Add a key called “birth_date” to the JSON-string at the same level as the “name” key.
 sample["company"]["employee"]["birth_date"] = "08/10/2003"
 # Save the dictionary as JSON to a file.
-with open("sample.json", 'w') as f:
-    json.dump(sampleJson, f, indent=4)
+print(json.dumps(sampleJson, indent=4))
