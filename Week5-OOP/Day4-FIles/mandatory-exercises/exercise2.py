f880ace5abacb00132f0a580d08fd585b1d43869
Avishay <avtwito@gmail.com>
2023-02-15 16:20:01 +0200

    trying to fix indentation

--- Week5-OOP/Day4-FIles/mandatory-exercises/exercise2.py
+++ Week5-OOP/Day4-FIles/mandatory-exercises/exercise2.py
@@ -1,15 +1,5 @@
 import json
-sampleJson = """{ 
-   "company":{ 
-      "employee":{ 
-         "name":"emma",
-         "payable":{ 
-            "salary":7000,
-            "bonus":800
-         }
-      }
-   }
-}"""
+sampleJson = """{ "company":{ "employee":{ "name":"emma","payable":{ "salary":7000,"bonus":800}}}}"""
 
 # Access the nested “salary” key from the JSON-string above.
 sample = json.loads(sampleJson)
@@ -17,4 +7,5 @@ print(sample["company"]["employee"]["payable"]["salary"])
 # Add a key called “birth_date” to the JSON-string at the same level as the “name” key.
 sample["company"]["employee"]["birth_date"] = "08/10/2003"
 # Save the dictionary as JSON to a file.
-print(json.dumps(sampleJson, indent=4))
+with open("sample.json", 'w') as f:
+    json.dump(sampleJson, f, indent=4)
